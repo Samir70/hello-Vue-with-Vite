784b253c8b20381995a202ebfca90df974fb01e5
Samir70 <sbetmouni@talktalk.net>
2021-10-29 12:04:11 +0100

    add query and reset button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,23 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String
-})
+// defineProps({
+//   msg: String
+// })
 
-const count = ref(0)
+const query = ref('')
+const reset = () => {
+  query.value = '' // clears the query
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement. Well?
-  </p>
+  <div>
+    <h2>Filter LearnVue Articles</h2>
+    <input type="text" placeholder="Filter Search" v-model="query" />
+    <p>{{ query }}</p>
+    <button v-on:click="reset">Reset</button>
+  </div>
 </template>
 
 <style scoped>
