d17a7b6cf97289be231f4b5b87193984b9bca824
Samir70 <sbetmouni@talktalk.net>
2021-10-29 16:02:42 +0100

    build without public in path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/hello-Vue-with-Vite/public/'
+  base: '/hello-Vue-with-Vite/'
 })
